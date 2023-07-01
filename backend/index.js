8fb7121c96e8ccd72b7d1cf2d59bc5cc1e055307
Sourabh30032003@ <sj07032003@gmail.com>
2023-07-01 17:30:25 +0530

    update the url

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.get('/',(req,res)=>{
  res.send('Hello World')
 })
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://sst-food.netlify.app/");
+    res.setHeader("Access-Control-Allow-Origin", "https://sst-food.netlify.app");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
